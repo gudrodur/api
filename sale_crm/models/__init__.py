c285bf3fcdd74b69e26472d9d52e3631beb9f954
gudrodur <gudrodur@gmail.com>
2025-04-07 10:25:29 +0000

    update

--- sale_crm/models/__init__.py
+++ sale_crm/models/__init__.py
@@ -2,6 +2,6 @@ from .base import Base
 from .users import UserDB
 from .contacts import ContactList
 from .calls import CallDB
-from .sales import Sale
-from .SaleContact import SaleContact
-from .status_models import ContactStatus, SaleStatus, SalesOutcome, ContactStatusEnum
+from .sales import Sale as SaleDB
+from sale_crm.models import SaleDB, UserDB, ContactList, SaleStatus, SalesOutcome
+
